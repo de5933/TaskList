c531da7f41bbba8269a6b69017034031ba7abe1e
ColinEgan <colinegan123@gmail.com>
2017-01-04 16:17:30 -0500

    Task View: static and json
    Static complete, JSON in progress.

---
+++ taskViewJSON.js
@@ -0,0 +1,18 @@
+var tasklist = [
+  {
+    title: 'Depressed Mosquito',
+    description: 'Talk to Alissa Kamerer and take the Flimsy Porcupine to Adventurer Beltway in Tyler',
+    checked: false,
+    tags: ['Channel', 'Cobra', 'Mouse']
+  },{
+    title: 'Unique Ostrich',
+    description: 'Go find Ashton Caulk and take the Labored Sloth to Sound Branch in Rockford',
+    checked: false,
+    tags: ['Salamander', 'Eel', 'Octopus']
+  },{
+    title: 'Arrogant Aardvark',
+    description: 'Rendezvous with Bruna Schmader and take the Psychotic Point to Sardine Crossing in Lafayette',
+    checked: false,
+    tags: ['Okapi', 'Elephant', 'Cove']
+  }
+];
